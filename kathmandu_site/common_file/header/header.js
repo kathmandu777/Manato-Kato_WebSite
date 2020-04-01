49537a5df16194fd75a095c57954ac99372a18ff
kathmandu777 <57817950+kathmandu777@users.noreply.github.com>
2020-04-01 18:02:42 +0900

    Add files via upload

---
+++ kathmandu_site/common_file/header/header.js
@@ -0,0 +1,18 @@
+$(function() {
+    /*
+        $('header ul li').hover(
+            function() {
+
+                //マウスカーソルが重なった時の処理
+                $(this).css('transform', 'rotate(5deg)');
+
+            },
+            function() {
+
+                //マウスカーソルが離れた時の処理
+                $(this).css('transform', 'rotate(0deg)');
+
+            }
+        );*/
+
+});
---
+++ kathmandu_site/profile/profile.js
@@ -0,0 +1,25 @@
+$(function() {
+    /*$("header").load("../common_file/header/header.html");*/
+
+    /*
+        $('.programming_languages_content .img').hover(
+            function() {
+
+                //マウスカーソルが重なった時の処理
+                $(this).fadeOut();
+                $(this).parent('li').children('p').fadeIn();
+
+            },
+            function() {
+
+                //マウスカーソルが離れた時の処理
+                $(this).fadeIn();
+                $(this).parent('li').children('p').fadeOut();
+
+            }
+        );*/
+
+    $("footer").load("../common_file/footer/footer.html");
+
+
+});
