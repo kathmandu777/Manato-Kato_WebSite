d3bcea2b7b52b7501ac716276e42ff719f320a90
kathmandu777 <57817950+kathmandu777@users.noreply.github.com>
2020-04-03 20:02:07 +0900

    header background-image change
    jsでヘッダーの背景画像と名言がリロードするたびに変更するようになった

--- kathmandu_site/common_file/header/header.js
+++ kathmandu_site/common_file/header/header.js
@@ -14,5 +14,106 @@ $(function() {
 
             }
         );*/
+    var num = Math.ceil(7 * Math.random());
+    var quote = [
+        [
+            "url('../../image/top_header_background3.jpg')",
+            "Done is better than perfect.",
+            "-by Mark Zuckerberg."
 
+        ],
+
+        [
+            "url('../../image/top_header_background5.jpg')",
+            "艱難汝を玉にす",
+            "-困難を乗り越えてこそ、立派な人間に成長する-",
+            {
+                p_fontsize: "13px"
+            }
+        ],
+
+        [
+            "url('../../image/top_header_background6.png')",
+            "No one has ever drowned in sweat.",
+            "-汗で溺れた者はいない-",
+            {
+                h3_fontsize: "18px",
+            },
+            {
+                h3_fontsize: "50px"
+            }
+        ],
+
+        [
+            "url('../../image/top_header_background7.jpg')",
+            "I’d rather have more heart than talent any day.",
+            "-才能よりも常に勇気をもっていたい-",
+            {},
+            {
+                h3_fontsize: "40px"
+            }
+
+        ],
+
+        [
+            "url('../../image/top_header_background8.jpg')",
+            "Let him who would move the world, first move himself.",
+            "-世界を動かそうと思ったら、まず自分自身を動かせ-",
+            {
+                p_fontsize: "12px"
+            },
+            {
+                h3_fontsize: "34px",
+                p_fontsize: "30px"
+            }
+        ],
+
+        [
+            "url('../../image/top_header_background9.jpg')",
+            "If you can dream it, you can do it.",
+            "-夢見ることができれば、それは実現できる-",
+            {
+                h3_fontsize: "18px",
+            },
+            {
+                h3_fontsize: "50px",
+                p_fontsize: "34px"
+            }
+        ],
+
+        [
+            "url('../../image/top_header_background10.jpg')",
+            "進まざる者は必ず退き、退かざる者は必ず進む",
+            "-福沢諭吉",
+            {
+                h3_fontsize: "15px",
+            },
+            {
+                h3_fontsize: "40px",
+            }
+        ]
+    ];
+    $('header').css('background-image', quote[num][0]);
+    $('#quote h3').text(quote[num][1]);
+    $('#quote p').text(quote[num][2]);
+    $('#quote h3').css("color", quote[num][3].h3_color);
+    $('#quote p').css("color", quote[num][3].p_color);
+
+    var $win = $(window);
+    $win.on('load resize', function() {
+        var windowWidth = $win.width();
+
+        if (windowWidth >= 960) {
+            // PCの処理
+            $('#quote h3').css("font-size", quote[num][4].h3_fontsize);
+            $('#quote p').css("font-size", quote[num][4].p_fontsize);
+
+        } else if (windowWidth >= 600) {
+            // TABの処理
+        } else {
+            // SPの処理
+            $('#quote h3').css("font-size", quote[num][3].h3_fontsize);
+            $('#quote p').css("font-size", quote[num][3].p_fontsize);
+        }
+    });
 });
