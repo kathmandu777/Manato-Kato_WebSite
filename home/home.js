a7470d7c570b3880f500267bdc6df982591f8b4c
kathmandu777 <57817950+kathmandu777@users.noreply.github.com>
2020-04-15 10:23:13 +0900

    Delete home.js

--- home/home.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    /*header 第三引数に無名関数を指定中*/
-    $("header").load("../common_file/header/header.html");
-    /*footer*/
-    $("footer").load("../common_file/footer/footer.html");
-});
