970a504e4c0d854ae0c87ed57fff5dde103125af
kathmandu777 <57817950+kathmandu777@users.noreply.github.com>
2020-04-19 11:12:12 +0900

    header bug commit

--- kathmandu_site/common_file/header/header.js
+++ kathmandu_site/common_file/header/header.js
@@ -91,16 +91,12 @@ $(function() {
     /*
         $('header ul li').hover(
             function() {
-
                 //マウスカーソルが重なった時の処理
                 $(this).css('transform', 'rotate(5deg)');
-
             },
             function() {
-
                 //マウスカーソルが離れた時の処理
                 $(this).css('transform', 'rotate(0deg)');
-
             }
         );*/
 
